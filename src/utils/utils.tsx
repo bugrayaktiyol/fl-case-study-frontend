3b96732e6cf38256015131fca16962fb75388739
Buğra Yaktıyol <bugrayaktiyol@gmail.com>
2024-01-08 03:18:25 +0300

    edit comment

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -5,9 +5,9 @@ import { ApiResponse, DataType } from "@/models/userModel";
 export const mapData = (data: ApiResponse): DataType => {
   return {
     key: data.ID.toString(),
-    Name: data.name,      // Use lowercase "name" instead of "Name"
-    Age: data.age,        // Use lowercase "age" instead of "Age"
-    Address: data.address, // Use lowercase "address" instead of "Address"
-    Tags: data.tags ? data.tags.split(',') : [], // Use lowercase "tags" instead of "Tags"
+    Name: data.name,
+    Age: data.age,
+    Address: data.address,
+    Tags: data.tags ? data.tags.split(',') : [],
   };
 };
